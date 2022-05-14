d2173687307013e10333e025f5267d103c81df0b
FaithD186 <faith.dong@mail.utoronto.ca>
2022-05-14 17:23:02 -0400

    Completed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,29 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let s = str.toLowerCase(); 
+    let newStr = s.replace(/[.,/#!$%^&*;:{}=-_`~()]/g, "").replace(/s{2,}/g, " ");
+
+    
+
+    
+    let i = 0;
+    let j = newStr.length - 1; 
+    while (i < j){
+        if (newStr[i] == ' '){
+            i++; 
+            continue;  
+              
+        }
+        if (newStr[j] == ' '){
+            j--; 
+            continue;
+        }
+        if (newStr[i].toLowerCase() != newStr[j].toLowerCase()){
+            return false;
+        }
+        i++;
+        j--; 
+    }
+    return true;
 
 };
 
