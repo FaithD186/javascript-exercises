7414a3a716b2847021015e059ea527aab7b21a8e
FaithD186 <faith.dong@mail.utoronto.ca>
2022-05-14 17:33:37 -0400

    Completed 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (parseInt(n) < 0){
+        return "OOPS";
+    }
+    if (parseInt(n) <2){
+        return parseInt(n);
+    }
+    return fibonacci(parseInt(n) - 1) + fibonacci(parseInt(n)-2); 
 
 };
 
