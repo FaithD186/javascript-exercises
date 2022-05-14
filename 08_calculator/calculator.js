747b8a5e80f917128749a7e025ea631611402ac4
FaithD186 <faith.dong@mail.utoronto.ca>
2022-05-14 16:49:30 -0400

    Completed 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
+const add = function(x, y) {
+  return x + y;
 	
 };
 
-const subtract = function() {
+const subtract = function(x, y) {
+  return x - y;
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+  let total = 0;
+  for (element of arr){
+    total = total +  element;
+  }
+  return total;
 	
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let total = arr[0]; 
+  for (let i = 1; i < arr.length; i++){
+    total = total * arr[i]; 
+  }
+  return total;
 
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  let total = 1; 
+	for (let i=0; i < y; i++){
+    total = total * x;
+  }
+  return total;
+
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if(x==0 || x ==1){
+    return 1;
+  }
+  else{
+    total = 1; 
+    for (let i =1; i < x; i++){
+      total = total * (i + 1);
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
